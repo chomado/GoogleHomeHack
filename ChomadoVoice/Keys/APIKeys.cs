3a68e91b354dfb0828c482c4aa6091f9cb702643
chomado (Madoka Chiyoda) <madoka.madoka@microsoft.com>
2018-08-22 03:26:34 +0900

    ダミーの APIKeys クラス追加

---
+++ ChomadoVoice/Keys/APIKeys.cs
@@ -0,0 +1,8 @@
+﻿namespace ChomadoVoice.Keys
+{
+    class APIKeys
+    {
+        // ここに、VoiceTextWebApiで自分で取得したAPIキーを入れてね！
+        public static readonly string VoiceTextWebApiKey = "(WebAPIキーを記入)";
+    }
+}
